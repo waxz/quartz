e21556fe348169dea8b444cfec8614c91d5bd926
waxz <waxz@github.com>
2025-04-06 09:51:44 +0000

    update http auth

--- run_quartz.sh
+++ run_quartz.sh
@@ -35,7 +35,8 @@ echo QUARTZ_DOMAIN $QUARTZ_DOMAIN
 echo QUARTZ_USER $QUARTZ_USER
 echo QUARTZ_PSW $QUARTZ_PSW
 
-sudo htpasswd -bcB -C 10 /etc/nginx/.htpasswd $QUARTZ_USER $QUARTZ_PSW
+if [ ! -f /etc/nginx/.htpasswd ]; then sudo htpasswd -bcB -C 10 /etc/nginx/.htpasswd $QUARTZ_USER $QUARTZ_PSW ; else sudo htpasswd -bB -C 10 /etc/nginx/.htpasswd $QUARTZ_USER $QUARTZ_PSW ;fi
+
 
 
 sudo cp $DIR/default.conf /etc/nginx/conf.d/default.conf
